2f783d81e867048f4d7171bb965ed0095dbad35e
alohamat <matalohazk@gmail.com>
2025-07-14 19:06:21 -0300

    first commit

---
+++ terminal.py
@@ -0,0 +1,14 @@
+# A script which downloads music from Spotify and video/music from Youtube.
+
+import spotdl as sp
+import yt_dlp
+import os
+
+def main():
+    pass
+
+def download(url, directory):
+    pass
+
+def change_directory():
+    pass
